54a3eb3d14e6f3630153ec0251d44d077722a735
xing.he <1245036094@qq.com>
2020-11-29 23:17:34 +0800

    chore: update webpack config & using cdn

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,12 +11,12 @@ module.exports = () => {
   return {
     mode: IS_DEV ? "development" : "production",
     entry: {
-      main: path.resolve(appRootPath, "src/index"),
+      main: path.resolve(appRootPath, "src/index")
     },
     output: {
       path: path.resolve(appRootPath, "dist"),
       filename: "[name].[hash:8].js",
-      chunkFilename: "[name].[chunkhash:8].js",
+      chunkFilename: "[name].[chunkhash:8].js"
     },
     devServer: {
       port: 3000,
@@ -24,24 +24,28 @@ module.exports = () => {
       contentBase: path.resolve(appRootPath, "public"),
       historyApiFallback: true, // true for index.html upon 404, object for multiple paths
       hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
-      https: false, // true for self-signed, object for cert authority
+      https: false // true for self-signed, object for cert authority
+    },
+    externals: {
+      react: "React",
+      "react-dom": "ReactDOM",
+      lunr: "lunr"
     },
     resolve: {
       extensions: [".wasm", ".mjs", ".js", ".jsx", ".ts", ".tsx", ".json"],
       alias: {
-        "@": path.resolve(appRootPath, "src"),
-      },
+        "@": path.resolve(appRootPath, "src")
+      }
     },
-
     module: {
       rules: [
         {
           test: /\.(jsx|js)$/,
-          use: ["babel-loader"],
+          use: ["babel-loader"]
         },
         {
           test: /\.(ts|tsx)$/,
-          use: ["babel-loader", "ts-loader"],
+          use: ["babel-loader", "ts-loader"]
         },
         {
           test: /\.(css|scss)$/,
@@ -53,37 +57,54 @@ module.exports = () => {
               loader: "postcss-loader",
               options: {
                 ident: "postcss",
-                plugins: () => [postcssNormalize()],
-              },
-            },
-          ],
+                plugins: () => [postcssNormalize()]
+              }
+            }
+          ]
         },
         {
           test: /\.(svg|png|jpe?g|gif)$/i,
           use: [
             {
-              loader: "file-loader",
-            },
-          ],
-        },
-      ],
+              loader: "file-loader"
+            }
+          ]
+        }
+      ]
     },
     optimization: {
       splitChunks: {
         chunks: "all",
-      },
+        cacheGroups: {
+          // lunr: {
+          //   name: "lunr",
+          //   test: /node_modules\/lunr/,
+          //   priority: 0
+          // },
+          vendors: {
+            name: "vendors",
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      }
     },
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         favicon: path.resolve(appRootPath, "public/favicon.ico"),
         meta: { appName: "Terms" },
-        template: path.resolve(appRootPath, "public/index.html"),
+        template: path.resolve(appRootPath, "public/index.html")
       }),
       new MiniCssExtractPlugin({
         filename: "[name].[contenthash:8].css",
-        chunkFilename: "[id].[contenthash:8].css",
-      }),
-    ],
+        chunkFilename: "[id].[contenthash:8].css"
+      })
+    ]
   };
 };
